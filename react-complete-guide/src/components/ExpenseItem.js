9c809756df6c7a350a1d988b88c5c69439a79949
unknown <ukeoonn@gmail.com>
2023-01-10 00:13:22 +0900

    컴포넌트 분할 하기

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,23 +1,17 @@
-import './ExpenseItem.css'
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
 
-function ExpenseItem(props) {   //props -> title , amount , date
-    const expenseDate = new Date(2021, 2, 28);
-    const expenseTitle = 'Car Insurance';
-    const expenseAmout = 294.67;
 
-    return (
+function ExpenseItem(props) {
+  return (
     <div className="expense-item">
-        <div>{expenseDate.toISOString()}</div>
-        <div className="expense-tiem__description">
-          <h2>{props.Title}</h2>
-          <div className='expense-item__price'>${props.amount}</div> 
-        </div>
+      <ExpenseDate date={props.date}/>
+      <div className="expense-item__description">
+        <h2>{props.Title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
     </div>
-    );
+  );
 }
 
 export default ExpenseItem;
-
-
-
-
