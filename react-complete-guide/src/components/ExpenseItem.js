c07b292a91e91472342c63015b1a4610392d969b
unknown <ukeoonn@gmail.com>
2023-01-09 22:27:38 +0900

    props

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,8 +1,23 @@
-function ExoebseItem(){
-    return <h2>Date</h2>
+import './ExpenseItem.css'
+
+function ExpenseItem(props) {   //props -> title , amount , date
+    const expenseDate = new Date(2021, 2, 28);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmout = 294.67;
+
+    return (
+    <div className="expense-item">
+        <div>{expenseDate.toISOString()}</div>
+        <div className="expense-tiem__description">
+          <h2>{props.Title}</h2>
+          <div className='expense-item__price'>${props.amount}</div> 
+        </div>
+    </div>
+    );
 }
 
-export default ExoebseItem;
+export default ExpenseItem;
+
 
 
 
